9e1eef1151274ce8e03dc917cdeaf74ac48f051e
Fazel Saeedi <fazelsaeedi@iran.ir>
2021-02-17 21:54:38 +0330

    implement Login form Js

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,20 +2,50 @@
   <h1>Login</h1>
   <p>
     <label class="sr-only" >Email Address</label>
-    <input class="form-control" type="email" placeholder="Email Address" required autofocus>
+    <input v-model="email" class="form-control" type="email" placeholder="Email Address" required autofocus>
   </p>
 
   <p>
     <label class="sr-only" >Password</label>
-    <input class="form-control" type="email" placeholder="Password" required>
+    <input v-model="password" class="form-control" type="email" placeholder="Password" required>
   </p>
 
-
-  <button class="btn btn-primary btn-block" type="sumbit">Login</button></template>
+  <button  @click="submit" class="btn btn-primary btn-block" type="button">Login</button></template>
 
 <script>
 export default {
-  name: "Login"
+  name: "Login",
+  data() {
+    return {
+      email: '',
+      password: '',
+    }
+  },
+  methods: {
+    submit: function () {
+
+      var user = localStorage.getItem(this.email)
+
+
+      user = JSON.parse(user)
+      console.log(user)
+
+      if (user) {
+        if (this.password === user.password && this.email === user.email) {
+
+          var userLogin = { email : this.email  };
+          localStorage.setItem('userlogin', JSON.stringify(userLogin));
+
+          alert("you are login")
+          this.$router.push('/')
+        }else
+          alert("your Password is wrong")
+      }
+      else
+        alert('Your UserName or Password is wrong')
+
+    }
+  }
 }
 </script>
 
