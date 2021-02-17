8438da9b2236f4222982ce96223268bf2268445e
Fazel Saeedi <fazelsaeedi@iran.ir>
2021-02-17 21:25:55 +0330

    implement Register form

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -2,24 +2,54 @@
   <h1>Register</h1>
   <p>
     <label class="sr-only" >Email Address</label>
-    <input class="form-control" type="email" placeholder="Email Address" required autofocus>
+    <input  v-model="email" class="form-control" type="email" placeholder="Email Address" required autofocus>
   </p>
 
   <p>
     <label class="sr-only" >Password</label>
-    <input class="form-control" type="email" placeholder="Password" required>
+    <input v-model="password" class="form-control" type="password" placeholder="Password" required>
   </p>
 
   <p>
     <label class="sr-only" >Confirm Password</label>
-    <input class="form-control" type="email" placeholder="Password" required>
+    <input v-model="confirmpassword" class="form-control" type="password" placeholder="Password" required>
   </p>
 
-  <button class="btn btn-primary btn-block" type="sumbit">Register</button></template>
+  <button @click="submit" class="btn btn-primary btn-block" type="button">Register</button></template>
 
 <script>
 export default {
-  name: "Register"
+  name: "Register" ,
+  data(){
+    return {
+      email : '',
+      password : '',
+      confirmpassword : ''
+    }
+  },
+  methods: {
+    submit: function(){
+
+      if (this.password === this.confirmpassword)
+      {
+        var user = {
+          email : this.email ,
+          password : this.password ,
+          onlineStatus : false
+        }
+
+        localStorage.setItem(this.email , JSON.stringify(user))
+
+        alert("You Registered successfully");
+
+        this.$router.push('Login')
+
+      }else{
+        alert("Your Password is not Confirmed")
+      }
+
+    }
+  }
 }
 </script>
 
