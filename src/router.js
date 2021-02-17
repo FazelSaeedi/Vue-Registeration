13bd4ba16ab607f28a9820ef69cfce98da217837
Fazel Saeedi <fazelsaeedi@iran.ir>
2021-02-17 20:58:03 +0330

    Create Register Page Component

--- src/router.js
+++ src/router.js
@@ -1,11 +1,17 @@
 import { createWebHistory, createRouter } from "vue-router";
 import Home from "@/views/Home";
+import Register from "@/views/Register";
 
 const routes = [
     {
         path: '/',
         name: 'Home',
         component: Home,
+    },
+    {
+        path: '/Register',
+        name: 'Register',
+        component: Register
     }
 ];
 
---
+++ src/views/Register.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1>Register</h1>
+  <p>
+    <label class="sr-only" >Email Address</label>
+    <input class="form-control" type="email" placeholder="Email Address" required autofocus>
+  </p>
+
+  <p>
+    <label class="sr-only" >Password</label>
+    <input class="form-control" type="email" placeholder="Password" required>
+  </p>
+
+  <p>
+    <label class="sr-only" >Confirm Password</label>
+    <input class="form-control" type="email" placeholder="Password" required>
+  </p>
+
+  <button class="btn btn-primary btn-block" type="sumbit">Sign In</button></template>
+
+<script>
+export default {
+  name: "Register"
+}
+</script>
+
+<style scoped>
+
+</style>
